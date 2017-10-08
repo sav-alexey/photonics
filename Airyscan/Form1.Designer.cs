7c27753aad721faccbcb2745906ef0e7ccf70141
sav-alexey <sav-alexey@mail.ru>
2017-10-08 23:47:44 +0300

    First commit

---
+++ Airyscan/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Airyscan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.ImeMode = System.Windows.Forms.ImeMode.KatakanaHalf;
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(110, 2);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(575, 492);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(300, 510);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(199, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 544);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 77);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(423, 544);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 77);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 656);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Airyscan/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Controls;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision;
+
+
+
+namespace Airyscan
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection videoDevices;
+        public Form1()
+        {
+            InitializeComponent();
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            for (int i = 1, n = videoDevices.Count; i <= n; i++)
+            {
+                string cameraName = i + " : " + videoDevices[i - 1].Name;
+
+                comboBox1.Items.Add(cameraName);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            StartCameras();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopCameras();
+        }
+
+        private void StopCameras()
+        {
+            videoSourcePlayer1.SignalToStop();
+            videoSourcePlayer1.WaitForStop();
+        }
+
+        private void StartCameras()
+        {
+            try
+            {
+
+                VideoCaptureDevice videoSource1 = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex].MonikerString);
+                videoSourcePlayer1.VideoSource = videoSource1;
+                videoSourcePlayer1.Start();
+            }
+            catch { MessageBox.Show("Выберете камеру"); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopCameras();
+        }
+    }
+}
